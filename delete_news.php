060b8147af39fbf8a2ddb3a5de3e933d7850773d
MahmoudShawqi <91689126+MahmoudShawqi@users.noreply.github.com>
2025-10-06 16:52:07 +0300

    Delete delete_news.php

--- delete_news.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-$conn = new mysqli("localhost", "root", "", "projectdb");
-
-$id = $_GET['id'];
-$conn->query("UPDATE news SET is_deleted = 1 WHERE id = $id");
-header("Location: view_news.php");
-?>
